e27b0a12b1bf57b85360d9432da3f5a4a5de6778
zekgt <78991517+zekgt@users.noreply.github.com>
2021-03-25 17:19:31 +0800

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+def function_1():
+   print("math calculator made by templar knight#3250")
+
+function_1()
+
+x = int(input("Number 1 > "))
+y = int(input("Number 2 > "))
+
+if function_1() == 1:
+  print("1. Add")
+  print("2. Subtract")
+  print("3. Multiply")
+  print("4. Divide")
+
+function_1() == 1
+z = print(int(input("> ")))
+
+print(z)
+
+if int(input(1)):
+  print("Enter Addends > ")
+
+def addition(added_value):
+ added_value = x + y
